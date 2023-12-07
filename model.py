188e4b8b31245ab93f8590916684d8aa8dbe9838
Ye Min Aung <97483341+ym-aung@users.noreply.github.com>
2023-12-07 15:12:55 -0800

    Update model.py
    Updated CNNModel layers

--- model.py
+++ model.py
@@ -51,14 +51,15 @@ class ImageDataset(Dataset):
 
   def __len__(self):
     return self.len
-  
+
+
 class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
-        self.convlayer1 = nn.Conv2d(3, 32, kernel_size=(5,5), stride=1, padding=0)
+        self.convlayer1 = nn.Conv2d(3, 32, kernel_size=(5,5), stride=1, padding=1)
         self.dropout = nn.Dropout(0.25)
         self.maxpool = nn.MaxPool2d(kernel_size=(2,2))
-        self.convlayer2 = nn.Conv2d(32, 32, kernel_size=(5,5), stride=1, padding=0)
+        self.convlayer2 = nn.Conv2d(32, 32, kernel_size=(5,5), stride=1, padding=1)
         self.layer1 = nn.Linear(28800, 512)
         self.fc = nn.Linear(512, 5)
 
@@ -66,17 +67,20 @@ class CNNModel(nn.Module):
         out = self.convlayer1(X)
         out = nn.functional.relu(out)
         out = self.dropout(out)
+        out = self.convlayer2(out)
+        out = nn.functional.relu(out)
         out = self.maxpool(out)
         out = torch.flatten(out, start_dim=1)
         out = self.layer1(out)
-        out = nn.functional.sigmoid(out)
         out = self.fc(out)
         return out
+
 
 def reset_weights(m):
   for i in m.children():
     if hasattr(i, 'reset_parameters'):
       i.reset_parameters()
+
 
 model = CNNModel()
 
@@ -86,10 +90,11 @@ learning_rate = 0.05
 num_epochs = 1
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+
 num_folds = 5
 kFold = StratifiedKFold(n_splits=num_folds, shuffle=True)
 splits = kFold.split(image_id_list, label_list)
-num_epochs = 25
+num_epochs = 50
 
 results = {}
 
@@ -127,7 +132,7 @@ for n,(train_index,test_index) in enumerate(splits):
       if (i+1) % 10 == 0:
         print ('Fold # {}, Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                 .format(n, epoch+1, num_epochs, i+1, total_step, loss.item()))
-  
+
   correct, all = 0,0
   with torch.no_grad():
     for i, data in enumerate(test_loader):
@@ -145,7 +150,7 @@ for n,(train_index,test_index) in enumerate(splits):
     print('Accuracy for fold %d: %d %%' % (n, 100.0 * correct / all))
     print('--------------------------------')
     results[n] = 100.0 * (correct / all)
-  
+
 # Print fold results
 print(f'K-FOLD CROSS VALIDATION RESULTS FOR {num_folds} FOLDS')
 print('--------------------------------')
